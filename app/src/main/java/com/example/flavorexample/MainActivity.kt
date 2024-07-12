2874888a26e772d55f12e2c34a9028b058d1aab7
wenjie <wenjie@thejoyrun.com>
2024-07-12 17:12:15 +0800

    多构建体的依赖方式

--- app/src/main/java/com/example/flavorexample/MainActivity.kt
+++ app/src/main/java/com/example/flavorexample/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.bumptech.glide.Glide
-import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
